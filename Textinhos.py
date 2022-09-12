6890e312b7b2fcd24fa37a82d9dccdd42b11b50b
victor-oli1999 <88786542+victor-oli1999@users.noreply.github.com>
2022-09-12 13:20:29 -0300

    Introdução e final

--- Textinhos.py
+++ Textinhos.py
@@ -1,10 +1,19 @@
 horario = input("(M)anhã, (T)arde ou (S)em: ")
-nome = input("Nome do cliente: ")
 
-if horario.upper() == "M":
-    print("Bom dia, " + nome)
-elif horario.upper() == "T":
-    print("Boa tarde, " + nome)
+if horario.upper() == "M" or horario.upper() == "T" or horario.upper() == "S":
+    nome = input("Nome do cliente: ")
+    if horario.upper() == "M":
+        cumprimentar = "\nBom dia, " + nome
+    elif horario.upper() == "T":
+        cumprimentar = "\nBoa tarde, " + nome
+    elif horario.upper() == "S":
+        cumprimentar = ""
+else:
+    exit()
 
-    
+
+print(cumprimentar + "\nTudo bem?\n")
+print("Qualquer dúvida estamos à disposição.\nAtenciosamente,\nVictor.")
+
+
 
